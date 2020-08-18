c5ac3d9747b39f7037f8b94744397bb39aa3e149
Jonathan Hirsch <jonat1996@gmail.com>
2020-08-17 22:58:38 -0700

    Changed notification process to rely on user-created notification strings, passed responsibility of handling errors to notificationListener

--- Server/src/main/java/controller/Controller.java
+++ Server/src/main/java/controller/Controller.java
@@ -1,11 +1,10 @@
 package controller;
 
-import enums.ContextType;
 import enums.FileType;
-import enums.Notification;
+import enums.ServerMethodType;
 import model.Context;
 import model.NetworkFile;
-import model.NetworkFolder;
+import model.requests.Notification;
 import org.json.JSONObject;
 import services.StreamingService;
 
@@ -19,6 +18,10 @@ Defines
 
 public class Controller extends ControllerManager {
 
+    private static final String STREAM_NOTIFICATION = "Stream Request Received";
+    private static final String GET_NOTIFICATION = "Get Data Request Received";
+    private static final String UPDATE_MODEL = "Update Folders and Files Received";
+    private static final String REFRESH_NOTIFICATION = "Refresh Notification Received";
 
     public static final String CACHE_FOLDER_IGNORE_STR = ".Caches";
     public static final String PATH_TO_CACHE_FOLDER = "/" + CACHE_FOLDER_IGNORE_STR;
@@ -40,12 +43,7 @@ public class Controller extends ControllerManager {
             ".webm", ".flv", ".avi", ".mpg", ".mkv", ".ts", ".srt"};
 
     private static final String [] foldersToIngore = {CACHE_FOLDER_IGNORE_STR};
-
-
     private String currentPath;
-    private int portNum;
-
-    private NetworkFolder root;
 
     public Controller() {
         currentPath = "";
@@ -72,11 +70,10 @@ public class Controller extends ControllerManager {
 
     public List<Context> createContextList(){
         List<Context> contextList = new ArrayList<>();
-
-        contextList.add(new Context(REFRESH_HANDLER_PATH, ContextType.REFRESH_CONTEXT));
-        contextList.add(new Context(GET_DATA_HANDLER_PATH, ContextType.GET_DATA_CONTEXT));
-        contextList.add(new Context(PATCH_FILE_FOLDER_PATH, ContextType.PATCH_FILE_FOLDER));
-        contextList.add(new Context(STREAM_PATH, ContextType.STREAMING_START_CONTEXT));
+        contextList.add(new Context(REFRESH_HANDLER_PATH, ServerMethodType.GET, new Notification(REFRESH_NOTIFICATION)));
+        contextList.add(new Context(GET_DATA_HANDLER_PATH, ServerMethodType.GET, new Notification(GET_NOTIFICATION)));
+        contextList.add(new Context(PATCH_FILE_FOLDER_PATH, ServerMethodType.PATCH, new Notification(UPDATE_MODEL)));
+        contextList.add(new Context(STREAM_PATH, ServerMethodType.POST, new Notification(STREAM_NOTIFICATION)));
         return contextList;
     }
 
@@ -86,16 +83,18 @@ public class Controller extends ControllerManager {
 
     @Override
     public void notificationReceived(Notification notification, Object obj, long id) {
-        switch (notification){
-            case REFRESH_CALLED -> handleRefresh(id);
-            case PATCH_WITH_HASH -> handlePatchWithHash(obj, id);
-            case FILE_STREAMING_REQUESTED -> handleFileStreamingRequested(obj, id);
-            case GET_CALLED -> handleGetData(id);
+
+        switch (notification.name()){
+            case STREAM_NOTIFICATION ->handleFileStreamingRequested(obj, id);
+            case GET_NOTIFICATION -> handleGetData(id);
+            case UPDATE_MODEL -> handlePatchWithHash(obj, id);
+            case REFRESH_NOTIFICATION -> handleRefresh(id);
+            default -> System.out.println("Incorrecto notification: " + notification.name() + " " +  obj.toString() + " " + id);
         }
     }
 
     public void handleFileStreamingRequested(Object obj, long id){
-        boolean requestProcessed = false;
+        int responseCode = -1;
         JSONObject response = new JSONObject();
 
         if (obj instanceof JSONObject requestObject){
@@ -107,17 +106,20 @@ public class Controller extends ControllerManager {
                 ss.addObserver(serverController);
                 ss.createContexts();
                 response = file.getJSONFile();
-                requestProcessed = true;
+                responseCode = 200;
             }else{
+                responseCode = 406;
                 response.put("message", "File with hash " + hash + "not found. Streaming not started");
             }
         }else{
+            responseCode = 400;
             response.put("message", "Invalid data format. Streaming not started");
         }
-        serverController.handleRequestResponse(id, response, requestProcessed);
+        serverController.handleRequestResponse(id, response, responseCode);
     }
 
     public void handlePatchWithHash(Object obj, long id){
+        int responseCode = -1;
         boolean updated = false;
         JSONObject response = new JSONObject();
         if (obj instanceof JSONObject requestObject){
@@ -132,42 +134,44 @@ public class Controller extends ControllerManager {
                 updated = dataController.updateFolderAtHash(hash, isFavorite);
             }
             if (updated){
+                responseCode = 200;
                 response.put("message", "object at " + hash + " updated");
             }else{
+                responseCode = 404;
                 response.put("message", "object with " + hash + "not found");
             }
         }else{
+            responseCode = 400;
             response.put("message", "invalid data update patch request");
         }
-        serverController.handleRequestResponse(id, response, updated);
+        serverController.handleRequestResponse(id, response, responseCode);
     }
 
     public void handleGetData(long id){
         JSONObject response = new JSONObject();
         response.put("message", "Returned Data from Network");
         response.put("folders", dataController.getJSONData());
-        serverController.handleRequestResponse(id, response,true);
+        serverController.handleRequestResponse(id, response,200);
     }
 
     public void handleRefresh(long id){
         for (Context context: createContextList()){
-            System.out.println("Removing context at path: " + context.getPath());
-            serverController.removeContext(context.getPath());
+            System.out.println("Removing context at path: " + context.path());
+            serverController.removeContext(context.path());
         }
 
         requestDataCreation();
 
-        serverController.createContexts(createContextList());
+        serverController.createContexts(createContextList().toArray(new Context[]{}));
         JSONObject response = new JSONObject();
         response.put("message", "Refresh Complete!");
         System.out.println(response.toString());
-        serverController.handleRequestResponse(id, response, true);
+        serverController.handleRequestResponse(id, response, 200);
 
     }
 
     @Override
     public void requestData() {
-        serverController.createContexts(createContextList());
+        serverController.createContexts(createContextList().toArray(new Context[]{}));
     }
-
 }
